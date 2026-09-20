2e57808a231e3d9058d9932aff8b395166c7c8e2
Seungwon32 <bsw030722@gmail.com>
2026-09-21 00:00:51 +0900

    [Chore] spring boot 초기 세팅

---
+++ src/main/java/likelion/yacha_backend/YachaBackendApplication.java
@@ -0,0 +1,13 @@
+package likelion.yacha_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YachaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/yacha_backend/YachaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.yacha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
